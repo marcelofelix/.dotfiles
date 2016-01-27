559f309edf3958356c13995cdb6c1bae8d21c510
Marcelo Felix <olivera.felix@gmail.com>
2016-01-27 16:33:03 -0200

    fix numbers of cpu to create docker machine

--- bash/functions.sh
+++ bash/functions.sh
@@ -1,5 +1,5 @@
 function create_dev_machine(){
-    docker-machine --driver=virtualbox --virtualbox-memory "4096" --virtualbox-cpu-count "1" dev;
+    docker-machine --driver=virtualbox --virtualbox-memory "4096" --virtualbox-cpu-count "3" dev;
 }
 
 function envdocker(){
