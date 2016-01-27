18cee05c4ea1bfc54351422a391f57d88bee4c29
Marcelo Felix <olivera.felix@gmail.com>
2016-01-27 09:49:59 -0200

    memory and cpu

--- bash/functions.sh
+++ bash/functions.sh
@@ -1,5 +1,5 @@
 function create_dev_machine(){
-    docker-machine --driver=virtualbox dev;
+    docker-machine --driver=virtualbox --virtualbox-memory "4096" --virtualbox-cpu-count "1" dev;
 }
 
 function envdocker(){
