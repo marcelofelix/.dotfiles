da728374488a5aa4f3ce9c146d924e39cd73d15c
Marcelo Felix <olivera.felix@gmail.com>
2016-04-25 09:16:00 -0300

    fix vim path

--- init.sh
+++ init.sh
@@ -11,7 +11,7 @@ bash "$DOTFILES_DIR"/tools.sh
 DOTFILES_HOME="$DOTFILES_DIR"/home/*
 for DOTFILE in $DOTFILES_HOME
 do
-	if [ -f "$DOTFILE" ]; then		
+	if [ -f "$DOTFILE" ]; then
 		rm "$HOME/.$(basename $DOTFILE)"
         cp -f "$DOTFILE" "$HOME/.$(basename $DOTFILE)"
 	fi
@@ -20,7 +20,7 @@ done
 DOTFILES_SOURCE="$DOTFILES_DIR"/source/*
 for DOTFILE in $DOTFILES_SOURCE
 do
-	if [ -f "$DOTFILE" ]; then		
+	if [ -f "$DOTFILE" ]; then
 		rm "$HOME/.$(basename $DOTFILE)"
         cp -f "$DOTFILE" "$HOME/.$(basename $DOTFILE)"
         . "$HOME/.$(basename $DOTFILE)"
@@ -30,12 +30,13 @@ done
 DOTFILES_SCRIPTS="$DOTFILES_DIR"/scripts/*
 for DOTFILE in $DOTFILES_SCRIPTS
 do
-	if [ -f "$DOTFILE" ]; then				
+	if [ -f "$DOTFILE" ]; then
         . "$DOTFILE"
 	fi
 done
 
 
+rm "$HOME"/.vim
 ln -s -f "$DOTFILES_DIR"/vim "$HOME"/.vim
 
 eval "$(rbenv init -)"
