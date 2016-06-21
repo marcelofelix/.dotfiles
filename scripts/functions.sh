2af3fe12666b1f036c30d98ff6f002c91b8aef52
Marcelo Felix <olivera.felix@gmail.com>
2016-06-21 08:46:22 -0300

    remove invalid functions

--- scripts/functions.sh
+++ scripts/functions.sh
@@ -2,10 +2,6 @@ function create_dev_machine(){
     docker-machine --driver=virtualbox --virtualbox-memory "4096" --virtualbox-cpu-count "3" dev;
 }
 
-function envdocker(){
-	eval $(docker-machine env dev)
-}
-
 function docker_clean_volumes(){
   docker run --rm -v /var/run/docker.sock:/var/run/docker.sock:ro -v /var/lib/docker:/var/lib/docker martin/docker-cleanup-volumes
 }
