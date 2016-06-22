a569d3a078fbb3e15e6681ade3aecb34da3d2046
Marcelo Felix <olivera.felix@gmail.com>
2016-06-22 14:16:40 -0300

    ignore directory in command-t

--- init.sh
+++ init.sh
@@ -39,6 +39,6 @@ done
 rm "$HOME"/.vim
 rm "$HOME"/.git_template
 ln -s -f "$DOTFILES_DIR"/vim "$HOME"/.vim
-ln -s -f "$DOTFILES_HOME"/git_template "$HOME"/.git_template
+ln -s -f "$DOTFILES_DIR/home"/git_template "$HOME"/.git_template
 
 eval "$(rbenv init -)"
