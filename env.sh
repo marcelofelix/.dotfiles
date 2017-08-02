bc4605dbf3d05abcd8943dbd18b4191fe32789d2
Marcelo Felix <olivera.felix@gmail.com>
2017-08-01 22:30:19 -0300

    update

--- env.sh
+++ env.sh
@@ -6,6 +6,6 @@ export JAVA_HOME=/usr/lib/jvm/java-8-openjdk
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
-export PATH="$HOME/.local/bin:$HOME/.rbenv/bin:$PATH"
+export PATH="~/.rbenv/shims:/usr/local/bin:/usr/bin:/bin"
 export PATH="$PATH:$HOME/Applications/idea/bin"
 eval "$(rbenv init -)"
